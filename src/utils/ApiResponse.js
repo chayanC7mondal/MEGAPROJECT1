e2fdc55a3514dffbecb5b3ad4c6e2496fbbf1a43
chayanc7mondal <mitalichayan18082004@gmail.com>
2024-11-04 00:34:49 +0530

    utils

---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,9 @@
+class ApiResponse{
+    constructor(statusCode, data, messag= "Success"){
+        this.statusCode = statusCode;
+        this.data = data;
+        this.message = message;
+        this.success = true;
+    }
+        
+}
