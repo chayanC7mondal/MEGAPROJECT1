a4e0fb4aafc2dd0111de641674a5358f1de47807
chayanc7mondal <mitalichayan18082004@gmail.com>
2024-11-03 18:16:05 +0530

    connect to mongo atlas

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-import mongoose from "mongoose";
+// require('dotenv').config();
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+
+
+dotenv.config({
+    path: ".env"
+});
+
+connectDB();
+
+console.log(process.env.MONGODB_URI);
+
+
+
+
+/*
+( async () => {
+    try{
+ await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}` )
+    }catch(error){
+        console.error("ERROR", error)
+        throw err
+    }
+})() */
