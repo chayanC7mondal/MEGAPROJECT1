2f6f1b00502cc17b2f3519df0595f3b6502d8295
chayanc7mondal <mitalichayan18082004@gmail.com>
2024-12-08 23:37:59 +0530

    new

--- src/app.js
+++ src/app.js
@@ -13,6 +13,13 @@ app.use(cors({
 app.use(express.json({limit:"16kb"}));
 app.use(express.urlencoded({extended: true, limit: "16kb"}));
 app.use(express.static("public"));
-app.use(cookieParser());
+app.use(cookieParser())
+
+
+//routes import
+
+import userRouter from "./routes/user.routes.js";
+// routes declaration
+app.use("/users", userRouter);
 
 export {app};
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -10,13 +10,12 @@
 //     }
 // }
 
+const asyncHandler = (requestHandler) => {
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
 
-const asyncHandler = (requestHandler) =>  {
-    (req,res,next) => {
-        Promise.resolve(requestHandler(req,res,next)).catch((err) => next(err));
-    }
+export { asyncHandler };
 
-}
-
-
-export {asyncHandler};
+// const asyncHandler = () => {}
