204a61bb2b8cfd6e4a0c4950f9f03515506c9849
chayanc7mondal <mitalichayan18082004@gmail.com>
2024-12-13 21:49:54 +0530

    debugged

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,8 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  response.status(200).json({
-    message: "ok",
+  res.status(200).json({
+    message: "takatak ok",
   });
 });
 
