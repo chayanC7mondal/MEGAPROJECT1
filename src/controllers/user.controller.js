b40585bad967be33f3fc53ee1f378e4479bfa4ae
chayanc7mondal <mitalichayan18082004@gmail.com>
2024-12-15 21:26:23 +0530

    user registration by postman

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,18 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "takatak ok",
-  });
+  //get user details from frontend
+  //validation- not empty
+  //check f user already exists: email,username
+  //check for images, check for avatar
+  //upload them to cloudinary, avatar
+  // create user object- create entry in db
+  //remove password and refresh token feild from response
+  //check for user creation
+  //return response
+
+  const { fullName, email, sername, password } = req.body;
+  console.log("email:", email);
 });
 
 export { registerUser };
