a0b315d645a9e98eff632ff652406a8cd3b690df
chayanc7mondal <mitalichayan18082004@gmail.com>
2024-12-15 22:10:34 +0530

    additions

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,7 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.models.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
 const registerUser = asyncHandler(async (req, res) => {
   //get user details from frontend
@@ -13,6 +16,27 @@ const registerUser = asyncHandler(async (req, res) => {
 
   const { fullName, email, sername, password } = req.body;
   console.log("email:", email);
+
+  if (
+    [fullName, email, username, password].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  const existedUser = User.findOne({
+    $or: [{ Username }, { email }],
+  });
+
+  if (existedUser) {
+    throw new ApiError(409, "User with this email or username already exists");
+  }
+
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar is required");
+  }
 });
 
 export { registerUser };
