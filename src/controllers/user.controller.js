2d5b8feeddef43a92f26d38628179ac12b3af4c4
chayanc7mondal <mitalichayan18082004@gmail.com>
2024-12-18 00:20:26 +0530

    middleware authentication logout and login

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -129,7 +129,7 @@ const loginUser = asyncHandler(async (req, res) => {
   );
 
   //cookie
-  
+
   const options = {
     httpOnly: true,
     secure: true,
@@ -138,13 +138,40 @@ const loginUser = asyncHandler(async (req, res) => {
     .status(200)
     .cookie("accessToken", accessToken, options)
     .cookie("refreshToken", refreshToken, options)
-    .json ApiResponse(200,
-      {
-        user: loggedInUser, accessToken, refreshToken
-
-      },
-      "User Logged In Successfully"
-    )
+    .json(
+      new ApiResponse(
+        200,
+        {
+          user: loggedInUser,
+          accessToken,
+          refreshToken,
+        },
+        "User Logged In Successfully"
+      )
+    );
 });
 
-export { registerUser, loginUser };
+const logoutUser = asyncHandler(async (req, res) => {
+  //clear cookies
+  User.findByIdAndUpdate(req.user._id, {
+    $set: {
+      refreshToken: undefined,
+    },
+  }
+{
+  new: true
+}
+);
+const options = {
+  httpOnly: true,
+  secure: true,
+};
+return res
+.status(200)
+.clearCookie("accessToken", options)
+.clearCookie("refreshToken", options)
+.json(new ApiResponse(200, {}, "User Logged Out Successfully"))
+
+});
+
+export { registerUser, loginUser, logoutUser };
--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,3 +1,33 @@
 import { asyncHandler } from "../utils/asyncHandler";
+import { ApiError } from "../utils/ApiError";
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.models";
 
-export const verifyJWT = asyncHandler(async (req, res, next) => {});
+//mo res then _
+export const verifyJWT = asyncHandler(async (req, _, next) => {
+  try {
+    const token =
+      req.cookies?.accessToken ||
+      req.header("Authorization")?.replace("Bearer ", "");
+
+    if (!token) {
+      throw new ApiError(401, "Unauthorized Request");
+    }
+
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+    const user = await User.findById(decodedToken?._id).select(
+      "-password -refreshToken"
+    );
+
+    //todo discuss about frontend
+
+    if (!User) {
+      throw new ApiError(401, "Invalid Access Token");
+    }
+    req.user = user;
+    next();
+  } catch (error) {
+    throw new ApiError(401, error?.message || "Invalid Access Token");
+  }
+});
